b527f3a135886aabe338cdc1507b448cc3017051
Raul Arribas <raul@exordo.com>
2025-07-06 12:27:50 +0100

    adds header to table

--- src/components/ui/modals/Dashboard.jsx
+++ src/components/ui/modals/Dashboard.jsx
@@ -1,9 +1,12 @@
 import { PageHeader } from "../PageHeader";
 import { headerColumns } from "@/components/TableConfiguration";
 import DataTable from "../DataTable";
+import Header from "../Header";
 import { useContext } from "react";
 import Panel from "../Panel";
+import { Button } from "../Button";
 import TableDataContext from "@/context/mainTableDataContext";
+import {Plus} from "lucide-react";
 
 export default function Dashboard(){
   const {tableData} = useContext(TableDataContext);
@@ -11,6 +14,13 @@ export default function Dashboard(){
     <>
       <PageHeader/>
       <Panel>
+        <Header className="p-4">
+          <p className="text-sm text-primary font-medium">Learn More</p>
+          <Button disabled={true}>
+            <Plus/>
+            Add Equipment
+          </Button>
+        </Header>
         <DataTable columns={headerColumns} data={tableData}/>
       </Panel>
     </>
